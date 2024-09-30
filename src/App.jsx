a51b42c2dd8f2430bc5275347132dedcda2005cf
pmc14 <pmcpatel@ucdavis.edu>
2024-09-29 23:31:43 -0700

    fixed tab name

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import JoinUs from './pages/JoinUs';
-import Projects from './pages/Projects';
-
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import JoinUs from "./pages/JoinUs";
+import Projects from "./pages/Projects";
+import { useEffect } from "react";
 
 function App() {
-
+  useEffect(() => {
+    document.title = "Neurotech@Davis";
+  }, []);
 
   return (
     <BrowserRouter>
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/joinus' element={<JoinUs />} />
-        <Route path='/projects' element={<Projects />} />
- 
-
-
-
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/joinus" element={<JoinUs />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
 
       <Footer />
-
-
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
