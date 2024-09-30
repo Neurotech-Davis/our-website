f7ea78d6669a34b32d5319499cb2d381deded336
pmc14 <pmcpatel@ucdavis.edu>
2024-09-29 22:30:51 -0700

    new try

---
+++ src/components/RolesData.js
@@ -0,0 +1,29 @@
+
+const RolesData = [
+    {
+        role: 'Project Member',
+        divison: 'Project Divison',
+        desc: 'Design and develop BCIs linking the brain to digital systems, from wearable devices to software',
+        link: 'https://forms.gle/JzCWGbojLkRwVTAL7'
+    },
+    {
+        role: 'Education Member',
+        divison: 'Education Divison',
+        desc: 'Lead programs in local high schools to inroduce students to the basics of neuroscience',
+        link: 'https://docs.google.com/forms/d/e/1FAIpQLSfibwKvu9CAMG5nlwkZLFoyJTbGRCaj6nj4rERpxapPuAgJlg/viewform?pli=1'
+    },
+    {
+        role: 'Publications Member',
+        divison: 'Education Publication',
+        desc: 'Actively contribute to a variety of publications in the field of neuroscience by sharing research findings, insights, and expert analyses',
+        link: 'https://docs.google.com/forms/d/e/1FAIpQLSefqbi4UvfwIPQlHE-yYHSpmjxURGGOjnTxAFdv0mRziRE_Fw/viewform'
+    },
+    {
+        role: 'Entrepreneurship Member',
+        divison: 'Entrepreneurship Divison',
+        desc: 'Engage in neurotech startups, we offer a platform to develop ideas and create solutions, compete in startup competitions ',
+        link: 'https://forms.gle/kuzH13rgDudbQtkVA'
+    }
+]
+
+export default RolesData;
