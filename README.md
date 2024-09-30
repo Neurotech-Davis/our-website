0042a400decd88810cced1be2cd4c964ce44c3e9
pmc14 <pmcpatel@ucdavis.edu>
2024-09-29 18:13:02 -0700

    new website

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to run the project
+1. clone repo locally
+2. enter "npm install" in terminal once in root directory
+3. then enter "npm run dev" in terminal 
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import JoinUs from './pages/JoinUs';
+import Projects from './pages/Projects';
+
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/joinus' element={<JoinUs />} />
+        <Route path='/projects' element={<Projects />} />
+ 
+
+
+
+      </Routes>
+
+      <Footer />
+
+
+    </BrowserRouter>
+  )
+}
+
+export default App
